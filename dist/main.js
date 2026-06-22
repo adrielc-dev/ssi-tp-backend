a1e1c4664d62c66c067a52dd024c4923ed04946e
adriel <adrielc.dev@gmail.com>
2026-06-22 13:37:23 -0300

    Corregimos url nueva

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,11 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: ['http://localhost:5173', 'http://localhost:4173'],
+        origin: [
+            'http://localhost:5173',
+            'http://localhost:4173',
+            'https://ssi-tp-frontend.onrender.com',
+        ],
         methods: ['GET', 'POST'],
         credentials: true,
     });
